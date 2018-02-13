aaa799e3a12c248f786eafa406404eeb5d5940c6
Ted Paulsen <ted.m.paulsen@gmail.com>
2018-02-12 20:20:05 -0600

    I did some things, now it is in better shape.

--- Lab2/Lab2/Lab2/main.asm
+++ Lab2/Lab2/Lab2/main.asm
@@ -8,7 +8,7 @@ sbi DDRB, 0 ; set PORTB, 0 to input (ser_in)
 ...
 ; display a digit
 ;ldi R16, 0b01100111 ; load pattern to display
-;rcall nine
+;rcall five
 ;rcall display ; call display subroutine
 rcall rotate ;goal: display all numbers starting at 0 going to 9
 
@@ -31,8 +31,6 @@ set_ser_in_1:
 	; put code here to set SER_IN to 1
 	sbi PORTB, 0 ; maybe
 
-
-
 end:
 	; put code here to generate SRCK pulse
 	sbi PORTB, 1 ; set srck high
@@ -46,7 +44,6 @@ end:
 	nop			 ; wait
 	cbi PORTB, 2 ; set rck low
 
-
 	; restore registers from stack
 	pop R17
 	out SREG, R17
@@ -54,66 +51,84 @@ end:
 	pop R16
 	ret
 
-rotate:;doesn't work as planned :/
+rotate: ;doesn't work as planned :/
 	rcall zero
 	rcall display
 	rcall delay
-	rcall one 
+
+	rcall one
 	rcall display
 	rcall delay
+
 	rcall two
+	rcall display
+	rcall delay
+
+	rcall three
+	rcall display
+	rcall delay
+
+	ret
 	;etc downto 9
 	
 
 zero:
 	ldi R16, 0b00111111
-	rjmp display
+	ret
+	;rjmp display
 	
 one:
 	ldi R16, 0b00000110
-	rjmp display
+	ret
+	;rjmp display
 	
 two:
 	ldi R16, 0b01011011
-	rjmp display
+	ret
+	;rjmp display
 	
 three:
 	ldi R16, 0b01001111
-	rjmp display
+	ret
+	;rjmp display
 	
 four:
 	ldi R16, 0b01100110
-	rjmp display
+	ret
+	;rjmp display
 	
 five:
 	ldi R16, 0b01101101
-	rjmp display
+	ret
+	;rjmp display
 	
 six:
 	ldi R16, 0b01111101
-	rjmp display
+	ret
+	;rjmp display
 	
 seven:
 	ldi R16, 0b00000111
-	rjmp display
+	ret
+	;rjmp display
 
 eight:
 	ldi R16, 0b01111111
-	rjmp display
+	ret
+	;rjmp display
 
 nine: 
 	ldi R16, 0b01100111
-	rjmp display
-
-
+	ret
+	;rjmp display
 
 
 delay:
-      ldi   r23, 10      ; r23 <-- Counter for outer loop
-  d1: ldi   r24,255     ; r24 <-- Counter for level 2 loop 
+      ldi   r23, 4      ; r23 <-- Counter for outer loop
+  d1: ldi   r24, 255    ; r24 <-- Counter for level 2 loop 
   d2: ldi   r25, 246    ; r25 <-- Counter for inner loop
   d3: dec   r25
-      nop               ; no operation 
+      nop               ; no operation
       brne  d3 
       dec   r24
       brne  d2
