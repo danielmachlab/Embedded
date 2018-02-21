75060fe45840ebfdb4ce1f13f1ffe71513f47698
danielmachlab <daniel@hadlim.com>
2018-02-21 15:39:13 -0600

    SET UP THE I/O DDRB

--- Lab3/Lab3/main.asm
+++ Lab3/Lab3/main.asm
@@ -2,11 +2,23 @@
 ;; Ted Paulsen, Daniel Machlab
 ;; Embedded Systems
 
-cbi DDRB, 0
-cbi DDRB, 1
-sbi DDRB, 2
+;cbi is input sbi is output
+cbi DDRB, 0 ; input - from A
+cbi DDRB, 1 ; input - from B
+sbi DDRB, 2 ; output - turn left(A side) LED
+sbi DDRB, 5 ; output - turn right(B side) LED
+
+
 
 loop:
 	inc R16
 	dec R16
 	rjmp loop
+
+infiniteloopcheckingforturnofRPG:
+	in R16, PINB
+	
+;	andi rpg, 	
+
+
+	rjmp infiniteloopcheckingforturnofRPG
