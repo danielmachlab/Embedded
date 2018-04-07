cd786d220104c8109d16fe3fe37cc7ef2baf330a
Ted Paulsen <ted.m.paulsen@gmail.com>
2018-04-07 14:51:44 -0500

    Created lab5 c project

---
+++ lab5/lab5/main.c
@@ -0,0 +1,23 @@
+/*
+ * lab5.c
+ *
+ * Created: 4/7/2018 2:23:34 PM
+ * Author : ted
+ */ 
+
+#ifndef F_CPU
+#define F_CPU 8000000UL // 8 MHz clock speed 
+#endif
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void) {
+    
+	/* Replace with your application code */
+    while (1) {
+		
+    }
+	
+}
+
