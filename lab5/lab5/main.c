ce90b526a3827313bd8fb9b67700ab1f1e5d4749
Ted Paulsen <ted.m.paulsen@gmail.com>
2018-04-09 10:19:15 -0500

    some c things

--- lab5/lab5/main.c
+++ lab5/lab5/main.c
@@ -9,15 +9,45 @@
 #define F_CPU 8000000UL		// 8 MHz clock speed 
 #endif
 
+#define BAUDRATE 19200
+#define UBRRVAL ((F_CPU/(BAUDRATE*16UL))-1)
+
+
 #include <avr/io.h>			// includes #defines for PORTC etc.
 #include <util/delay.h>		// includes some pre-made delay routines
 
+void sei() {
+	
+}
+
+void usart_init() {
+	UBRRL = UBRRVAL;
+	UBRRH = (UBRRVAL>>8);
+	
+	UCSRC = (1<<URSEL)|(0<<UMSEL)|(0<<UPM1)|(0<<UPM0)|(0<<USBS)|(0<<UCSZ2)|(1<<UCSZ1)|(1<<UCSZ0);
+	UCSRB = (1<<RXEN)|(1<<TXEN)
+}
+
+void usart_prints(sdata) {
+	
+}
+
+void usart_printf(fdata) {
+	
+}
+
 int main(void) {
-    
-	/* Replace with your application code */
-    while (1) {
-			
-    }
+	
+	unsigned char c;
+	char str[25];
+	int adH, adL, dac;
+	int i;
+	
+	sei();
+	
+	usart_init();
+	usart_prints();
+	usart_printf();
 	
 }
 
