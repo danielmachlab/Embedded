89b36af07bb38fc6e8b0376e4bdaf0f1001d840c
Ted Paulsen <ted.m.paulsen@gmail.com>
2018-04-11 13:36:35 -0500

    created function outlines

--- lab5/lab5/main.c
+++ lab5/lab5/main.c
@@ -1,53 +1,209 @@
-/*
- * lab5.c
- *
- * Created: 4/7/2018 2:23:34 PM
- * Author : ted
- */ 
+/************************************************************************/
+/* UART functions written by A. Kruger 2010                             */
+/************************************************************************/
 
-#ifndef F_CPU
-#define F_CPU 8000000UL		// 8 MHz clock speed 
-#endif
+#define F_CPU 8000000L
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/pgmspace.h>
+#include <util/delay.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-#define BAUDRATE 19200
-#define UBRRVAL ((F_CPU/(BAUDRATE*16UL))-1)
+// Baud rate. The usart_int routine
+#define BAUD_RATE 9600
 
+// Variables and #define for the RX ring buffer.
+#define RX_BUFFER_SIZE 64
+unsigned char rx_buffer[RX_BUFFER_SIZE];
+volatile unsigned char rx_buffer_head;
+volatile unsigned char rx_buffer_tail;
 
-#include <avr/io.h>			// includes #defines for PORTC etc.
-#include <util/delay.h>		// includes some pre-made delay routines
+// Function prototypes
+float getSingleADC();
+float * getMultipleADC(int n, int dt);
+void setADC(int c, float v);
 
-void sei() {
-	
-}
+// UART function prototypes.
+unsigned char uart_buffer_empty(void);
+void usart_prints(const char *ptr);
+void usart_printf(const char *ptr);
+void usart_init(void);
+void usart_putc(const char c);
+unsigned char usart_getc(void);
 
-void usart_init() {
-	UBRRL = UBRRVAL;
-	UBRRH = (UBRRVAL>>8);
-	
-	UCSRC = (1<<URSEL)|(0<<UMSEL)|(0<<UPM1)|(0<<UPM0)|(0<<USBS)|(0<<UCSZ2)|(1<<UCSZ1)|(1<<UCSZ0);
-	UCSRB = (1<<RXEN)|(1<<TXEN)
-}
-
-void usart_prints(sdata) {
-	
-}
-
-void usart_printf(fdata) {
-	
-}
+// sample strings in SRAM and Flash, used for examples.
+const char sdata[40] = "Hello World!\n\r";          // String in SRAM
+const char fdata[] PROGMEM = "My name is Ted\n";  // String in Flash
 
 int main(void) {
-	
 	unsigned char c;
 	char str[25];
-	int adH, adL, dac;
-	int i;
-	
+
 	sei();
-	
+
 	usart_init();
-	usart_prints();
-	usart_printf();
-	
+	const char msg[] = "I now h v an iphone\n\r";
+	usart_prints(msg);
+
+	// listen for user input
+	while (1) {
+		c = usart_getc();
+
+		if (c == 'G') {
+			char response[] = "you typed G\n\r";
+			usart_prints(response);
+			float v = getSingleADC();
+			// todo: do something with this v
+
+		} else if (c == 'M') {
+			char response[] = "you typed M\n\r";
+			usart_prints(response);
+
+			int n = 5; // todo: read these values instead of hard coding
+			int dt = 10;
+			float *v = getMultipleADC(n, dt); // v is a pointer to the first element in a float array
+			// todo: do something with v
+
+		} else if (c == 'S') {
+			char response[] = "you typed S\n\r";
+			usart_prints(response);
+
+			int channel = 0; // todo: read these values instead of hard coding
+			int voltage = 3;
+			setADC(channel, voltage);
+		}
+
+	}
+
 }
 
+// get a single voltage measurement from ADC
+// return: the current ADC voltage
+float getSingleADC() {
+	float a = 0.0;
+
+	// todo
+
+	return a;
+}
+
+// get multiple measurements from ADC
+// n: number of measurements (2 <= n <= 20)
+// dt: time between measurements (1 <= dt <= 60 seconds)
+// return: pointer to the first element in an array of voltage readings
+float * getMultipleADC(int n, int dt) {
+	// allocate space for an array of n floats
+	float *arr = malloc(sizeof(float) * n);
+
+	// todo
+
+	return arr;
+}
+
+// set DAC output voltage
+// c: DAC channel number (0 or 1)
+// v: output voltage
+void setADC(int c, float v) {
+	// todo
+}
+
+// UART receive interrupt handler.
+// To do: check and warn if buffer overflows.
+ISR(USART_RX_vect) {
+	char c = UDR0;
+	rx_buffer[rx_buffer_head] = c;
+
+	if (rx_buffer_head == RX_BUFFER_SIZE - 1) {
+		rx_buffer_head = 0;
+		} else {
+		rx_buffer_head++;
+	}
+}
+
+// Configures the USART for serial 8N1 with
+// the Baud rate controlled by a #define.
+void usart_init(void) {
+	unsigned short s;
+
+	// Set Baud rate, controlled with #define above.
+
+	s = (double)F_CPU / (BAUD_RATE*16.0) - 1.0;
+	UBRR0H = (s & 0xFF00);
+	UBRR0L = (s & 0x00FF);
+
+	// Receive complete interrupt enable: RXCIE0
+	// Receiver & Transmitter enable: RXEN0,TXEN0
+
+	UCSR0B = (1<<RXCIE0)|(1<<RXEN0)|(1<<TXEN0);
+
+	// Along with UCSZ02 bit in UCSR0B, set 8 bits
+
+	UCSR0C = (1<<UCSZ01)|(1<<UCSZ00);
+
+	DDRD |= (1<< 1);         // PD0 is output (TX)
+	DDRD &= ~(1<< 0);        // PD1 is input (Rx)
+
+	// Empty buffers
+
+	rx_buffer_head = 0;
+	rx_buffer_tail = 0;
+}
+
+// Send NULL-terminated data from FLASH.
+// Uses polling (and it blocks).
+void usart_printf(const char *ptr) {
+	char c;
+	while(pgm_read_byte_near(ptr)) {
+		c = pgm_read_byte_near(ptr++);
+		usart_putc(c);
+	}
+}
+
+// Send "c" via the USART.  Uses poling
+// (and it blocks). Wait for UDRE0 to become
+// set (=1), which indicates the UDR0 is empty
+// and can accept the next character.
+void usart_putc(const char c){
+	while (!(UCSR0A & (1<<UDRE0)))
+	;
+
+	UDR0 = c;
+}
+
+
+// Send NULL-terminated data from SRAM.
+// Uses polling (and it blocks).
+void usart_prints(const char *ptr) {
+	while(*ptr) {
+		while (!( UCSR0A & (1<<UDRE0)))
+		;
+
+		UDR0 = *(ptr++);
+	}
+}
+
+// Get char from the receiver buffer.  This
+// function blocks until a character arrives.
+unsigned char usart_getc(void) {
+	unsigned char c;
+
+	// Wait for a character in the buffer.
+	while (rx_buffer_tail == rx_buffer_head)
+	;
+
+	c = rx_buffer[rx_buffer_tail];
+
+	if (rx_buffer_tail == RX_BUFFER_SIZE-1)
+	rx_buffer_tail = 0;
+	else
+	rx_buffer_tail++;
+
+	return c;
+}
+
+// Returns TRUE if receive buffer is empty.
+unsigned char uart_buffer_empty(void) {
+	return (rx_buffer_tail == rx_buffer_head);
+}
